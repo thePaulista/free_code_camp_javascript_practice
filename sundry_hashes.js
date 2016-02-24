1b122cb9fc5cc2a9ed7b7ef732ab20f42513ae56
Hedy <hedywoo@gmail.com>
2016-02-23 20:19:13 -0700

    javascript hashes and zika

---
+++ sundry_hashes.js
@@ -0,0 +1,73 @@
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.name = "Happy Camper";
+
+var myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["Free Code Camp Campers"]
+};
+
+myDog.name = "Happy Coder";
+
+// ********
+//adding additional keys and values
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.bark = "bow-wow";
+
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["Free Code Camp Campers"]
+};
+
+myDog.bark = "woof";
+//*********
+// delete attributes
+
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["Free Code Camp Campers"],
+  "bark": "woof"
+};
+
+delete myDog.tails;
+
+//bypassing the usageof switch and if/else by using hashes to look up value
+
+function phoneticLookup(val) {
+  var result = "";
+
+  var phonetics = {
+    "alpha": "Adams",
+    "bravo": "Boston",
+    "charlie": "Chicago",
+    "delta": "Denver",
+    "echo":"Easy",
+    "foxtrot": "Frank"
+  }
+
+    result = phonetics[val]
+  return result;
+}
+
+phoneticLookup("charlie");
+phoneticLookup("delta");
+phoneticLookup("echo");
