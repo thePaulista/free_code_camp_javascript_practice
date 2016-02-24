f91635b5c073d426e210e00ee17a931b50a378ea
Hedy <hedywoo@gmail.com>
2016-02-23 21:07:07 -0700

    first json code challenge

---
+++ intro_to_json.js
@@ -0,0 +1,40 @@
+// accessing nested JSON hashes. Access glove box.
+
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+var gloveBoxContents = myStorage.car.inside["glove box"];
+
+//accessing nested arrays in json. access "pines":
+// Setup
+var myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+// Only change code below this line
+
+var secondTree = myPlants[1].list[1]; // Change this line
